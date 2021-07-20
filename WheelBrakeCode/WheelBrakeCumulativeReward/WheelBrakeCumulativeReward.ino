a87bb4b1c5716e5fca05af3ec9117d451a2a96b3
dprotter <david.protter@gmail.com>
2021-07-20 11:38:33 -0600

    rewrote cum ulative rewarward on christians computer

--- WheelBrakeCode/WheelBrakeCumulativeReward/WheelBrakeCumulativeReward.ino
+++ WheelBrakeCode/WheelBrakeCumulativeReward/WheelBrakeCumulativeReward.ino
@@ -44,32 +44,26 @@ void loop(){
   // check if the sensor beam is broken
   // if it is, the IRread is LOW:
   if (IRread1 == LOW) {
-    delay(100);
-    while (IRread1 == LOW){
-        cumulative_time_1 += 100;
-        if (cumulative_time_1 >= reward_time){
-          cumulative_time_1 = 0;
-          brakeServo1.write(brakeRelease);
-          delay(2000); //wait for the animal to fall to the ramp
-          brakeServo1.write(brakeStop);  //stop the wheel
-        }
-        
-    }
     
-  } 
+    cumulative_time_1 += 100;
+    if (cumulative_time_1 >= reward_time){
+      cumulative_time_1 = 0;
+      brakeServo1.write(brakeRelease);
+      delay(2000); //wait for the animal to fall to the ramp
+      brakeServo1.write(brakeStop);  //stop the wheel
+        }
+    }
+     
 
   if (IRread2 == LOW) {
-    delay(100);
-    while (IRread2 == LOW){
-        cumulative_time_2 += 100;
-        if (cumulative_time_2 >= reward_time){
-          cumulative_time_2 = 0;
-          brakeServo2.write(brakeRelease);
-          delay(2000); //wait for the animal to fall to the ramp
-          brakeServo2.write(brakeStop);  //stop the wheel
-        }
-        
-    }
-    
+
+      cumulative_time_2 += 100;
+      if (cumulative_time_2 >= reward_time){
+        cumulative_time_2 = 0;
+        brakeServo2.write(brakeRelease);
+        delay(2000); //wait for the animal to fall to the ramp
+        brakeServo2.write(brakeStop);  //stop the wheel
+      }
   } 
+  delay(100);
 }
